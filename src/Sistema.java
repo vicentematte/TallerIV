0b7203dbc55f10792c0e091d981ef05519fc0c07
vicentematte <vicente.matte@alumnos.ucn.cl>
2026-06-10 14:11:10 -0400

    Implementacion de factory para crear cartas

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,10 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sistema {
-	private ArrayList<Carta> listaCartas = new ArrayList<>();
 	private static Sistema instancia;
 	
 	private Sistema() { //constructor
@@ -29,27 +27,9 @@ public class Sistema {
 			
 			while(s.hasNextLine()) {
 				String linea = s.nextLine();
-				String[] partes = linea.split(";");
+				String[] datos = linea.split(";");
 				
-				String nombreCarta = partes[0];
-				int rareza = Integer.parseInt(partes[1]);
-				String tipo = partes[2];
-				
-				//Dependiendo del "Tipo" de carta que sea, la guardaremos en la arraylist con su respectivo constructor.
-				if (tipo.equalsIgnoreCase("Pokemon")) {
-					Pokemon p = new Pokemon(nombreCarta,rareza,tipo,Integer.parseInt(partes[3]),Integer.parseInt(partes[4]));
-					listaCartas.add(p);
-				}else if (tipo.equalsIgnoreCase("Item")) {
-					Item i = new Item(nombreCarta,rareza,tipo,Integer.parseInt(partes[3])); 
-					listaCartas.add(i);
-				}else if (tipo.equalsIgnoreCase("Supporter")) {
-					Supporter sup = new Supporter(nombreCarta,rareza,tipo,Integer.parseInt(partes[3])); 
-					listaCartas.add(sup);
-				}else if (tipo.equalsIgnoreCase("Energy")) {
-					Energy e = new Energy(nombreCarta,rareza,tipo,partes[3]);
-					listaCartas.add(e);
-					
-				}
+				Carta carta = CartaFactory.crearCarta(datos);
 				
 			}
 			
